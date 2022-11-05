5be6d570c2b9957ba46ffbbed3069bb2a03f2a8a
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-11-04 22:10:27 -0300

    ( ͡≖ ͜ʖ ͡≖)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("\nínicio:");
+int numeroInicial = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nFim:");
+int numeroFinal = Convert.ToInt32(Console.ReadLine());
+
+if (numeroFinal < numeroInicial)
+{
+    Console.WriteLine("mano... faz isso direito!");
+}
+else
+{
+    while (numeroInicial <= numeroFinal)
+    {
+        Console.Write($"{numeroInicial} ");
+        numeroInicial +=1;
+    }
+}
+Console.WriteLine();
